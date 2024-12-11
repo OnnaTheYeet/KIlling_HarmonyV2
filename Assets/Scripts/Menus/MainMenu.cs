4c19b365056e27365e049f0eb45393e394cfb4a7
OnnaTheYeet <115497610+OnnaTheYeet@users.noreply.github.com>
2024-12-11 10:37:28 +0100

    Asset Inplementierung

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
