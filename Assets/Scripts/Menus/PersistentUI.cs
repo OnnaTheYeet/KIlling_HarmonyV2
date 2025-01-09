603a5966865a33031c7708b9ab17f8efd0e8f597
XinMarius <167301664+XinMarius@users.noreply.github.com>
2025-01-09 11:39:22 +0100

    BsP fix

--- Assets/Scripts/Menus/PersistentUI.cs
+++ Assets/Scripts/Menus/PersistentUI.cs
@@ -1,18 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PersistentUI : MonoBehaviour
 {
-    private static PersistentUI instance;
+    public string category = "Default";
+
+    private static Dictionary<string, PersistentUI> instances = new Dictionary<string, PersistentUI>();
 
     private void Awake()
     {
-        if (instance != null && instance != this)
+        if (instances.ContainsKey(category))
         {
             Destroy(gameObject);
             return;
         }
 
-        instance = this;
+        instances[category] = this;
         DontDestroyOnLoad(gameObject);
     }
 }
